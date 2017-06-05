07271fb93a6f113a4d5ced7e865e3adb300c6708
leeej <leeej9201@gmail.com>
2017-06-05 11:34:12 +0900

    BackEnd(Server) 작업완료 Todo vo class 생성 Todo Controller 생성 Todo Service 생성 Todo Dao 작업 Todo Sqls 작업 Todo DaoTest 생성

--- server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
@@ -1,15 +1,29 @@
 package kr.or.connect.todo.persistence;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import kr.or.connect.domain.Todo;
 
 @Repository
 public class TodoDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private SimpleJdbcInsert insertAction;
+	private RowMapper<Todo> rowMapper = BeanPropertyRowMapper.newInstance(Todo.class);
 	
 	public TodoDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
@@ -17,4 +31,47 @@ public class TodoDao {
 				.withTableName("todo")
 				.usingGeneratedKeyColumns("id");
 	}
+	
+	public int countTodos(){
+		Map<String, Object> params = Collections.emptyMap();
+		return jdbc.queryForObject(TodoSqls.COUNT_TODO, params, Integer.class);
+	}
+	
+	public Integer insert(Todo todo){
+		SqlParameterSource params = new BeanPropertySqlParameterSource(todo);
+		return insertAction.executeAndReturnKey(params).intValue();
+	}
+	
+	public Todo selectById(Integer id) {
+		rowMapper = new RowMapper<Todo>() {
+			@Override
+			public Todo mapRow(ResultSet rs, int i) throws SQLException {
+				Todo todo = new Todo();
+				todo.setId(rs.getInt("id"));
+				todo.setTodo(rs.getString("todo"));
+				todo.setCompleted(rs.getInt("completed"));
+				todo.setDate(rs.getDate("date"));
+				return todo;
+			}
+		};
+
+		Map<String, Object> params = new HashMap<>();
+		params.put("id", id);
+		return jdbc.queryForObject(TodoSqls.SELECT_BY_ID, params, rowMapper);
+	}
+	
+	public List<Todo> selectAll(){
+		Map<String, Object> params = Collections.emptyMap();
+		return jdbc.query(TodoSqls.SELECT_ALL, params, rowMapper);
+	}
+	
+	public int update(Todo todo) {
+		SqlParameterSource params = new BeanPropertySqlParameterSource(todo);
+		return jdbc.update(TodoSqls.UPDATE, params);
+	}
+	
+	public int deleteById(Integer id) {
+		Map<String, ?> params = Collections.singletonMap("id", id);
+		return jdbc.update(TodoSqls.DELETE_BY_ID, params);
+}
 }
